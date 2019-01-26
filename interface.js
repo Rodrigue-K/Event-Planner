359b89c47a2924ba81695c110b49a7920fe31583
Rodrigue  Kayembe <rodriguekayembe@gmail.com>
2019-01-26 16:02:10 +0000

    Added weather API using XMLHttpRequest. Looking to change to fetch method

--- interface.js
+++ interface.js
@@ -14,8 +14,9 @@ window.addEventListener('load', () => {
     console.log(event);
 
     display();
-  
   })
+  
+  
 
 
   const display = () => {
@@ -26,7 +27,52 @@ window.addEventListener('load', () => {
     console.log(events)
   }
 
-  
+  let weather = document.getElementById('weatherButton');
+  let city = document.getElementById('weatherText');
+  const apiKey = "8df399cfa3f4d13005c3c51f9949e550"
+  const apistring = "https://api.openweathermap.org/data/2.5/weather?q="
 
 
-}) 
+
+
+  weather.addEventListener("click", findweatherdetails);
+
+  function findweatherdetails() {
+    queryString = apistring + city.value + "&appid=" + apiKey
+    console.log(queryString)
+
+    var request= new XMLHttpRequest();
+    request.onreadystatechange = function() {
+      if(this.readyState == 4 && this.status == 200) {
+        let jsonObject = JSON.parse(request.response);
+        weatherDiv.innerHTML = parseInt(jsonObject.main.temp - 273);
+      }
+    }
+    
+    request.open('GET', queryString, true);
+    request.send();
+
+
+
+  //   fetch(queryString)
+  //     .then((response) => response.json)
+  //     .then(
+  //       function(response) {
+  //         if (response.status !== 200) {
+  //           console.log('Looks there was a problem. Status Code: ' + 
+  //             response.status);
+  //             return;
+  //         }
+  //         // Examine the response text
+  //         let jsonObject = JSON.parse(response);
+  //         weatherDiv.innerHTML = parseInt(jsonObject.main.temp - 273 );
+  //       }
+  //     )
+  //     .catch(function(err) {
+  //       console.log('Fetch Error :-S, err');
+    // }) 
+  }
+
+
+
+});
